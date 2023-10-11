4cf631b60307f690d76666d1f6f4cdef69d4eee4
Denis <ftforest680@gmail.com>
2023-10-11 22:53:26 +0700

    fix LogOut

--- src/components/MainBlock/SideBar/LogOut/LogOut.js
+++ src/components/MainBlock/SideBar/LogOut/LogOut.js
@@ -3,20 +3,21 @@ import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { ReactComponent as ArrowLeftIcon } from '../../../../assets/images/left-arrow.svg';
 import './LogOut.css';
+import {logOut} from "../../../../store/slices/auth";
 
 export const LogOut = () => {
   const history = useHistory();
 
   const dispatch = useDispatch();
 
-  const logOut = () => {
+  const logOut2 = () => {
     dispatch(logOut());
     history.push('/login');
   }
 
   return (
     <section className='sidebarBottom'>
-      <button onClick={logOut}>
+      <button onClick={logOut2}>
         <ArrowLeftIcon height={25} width={25} />
         <span>Выход</span>
       </button>
